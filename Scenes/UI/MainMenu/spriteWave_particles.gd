ce6fbd02b23e267a39aa78432c2f16cfb1306848
lrx5 <143709045+lrx5@users.noreply.github.com>
2024-09-30 18:51:02 +0800

    Shader Particle Fix
    - Fixed Shader for the particles behind the smoke

--- Scenes/UI/MainMenu/spriteWave_particles.gd
+++ Scenes/UI/MainMenu/spriteWave_particles.gd
@@ -5,8 +5,7 @@ var time = 0.0
 func _ready() -> void:
 	#Set Shader Variables
 	material.set_shader_parameter("amplitude", 5.0)
-	material.set_shader_parameter("frequency", 10.0)
-	material.set_shader_parameter("padding", 0.0)
+	material.set_shader_parameter("frequency", 100.0)
 func _process(delta):
 	time += delta 
 	material.set_shader_parameter("time", time)
