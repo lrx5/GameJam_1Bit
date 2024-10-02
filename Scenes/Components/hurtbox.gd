a4879fd64fd25f51d94449d97d32a2ec4de3a734
lrx5 <143709045+lrx5@users.noreply.github.com>
2024-10-02 12:59:58 +0800

    Effects Update
    - Incorporated Black to White Fade Shader
    - Added OnHit SFX

--- Scenes/Components/hurtbox.gd
+++ Scenes/Components/hurtbox.gd
@@ -11,6 +11,18 @@ extends Area2D
 var character = self
 var immunityCounter : float = 0
 
+#region New OnHit Variables - TESTING
+@onready var damage_particles: GPUParticles2D = $DamageParticles
+@onready var damage_particles_timer: Timer = $DamageParticlesTimer
+@onready var enemy_sprite: Sprite2D = $"../EnemySprite"
+@onready var on_hit_sfx: AudioStreamPlayer = $"../onHitSFX"
+var sfx_list = [
+	preload("res://Assets/Sounds/Sound Effects/OnHit/damaged1.wav"),
+	preload("res://Assets/Sounds/Sound Effects/OnHit/damaged2.wav"),
+	preload("res://Assets/Sounds/Sound Effects/OnHit/damaged3.wav"),
+]
+#endregion
+
 
 func _ready() -> void:
 	_getCharacter()
@@ -23,6 +35,10 @@ func _process(delta: float) -> void:
 func receiveDamage(damage: float):#, knockback: float):
 	if not immune:
 		health.currentHealth -= damage
+		#region Call OnHit Effects - TESTING
+		onDamagedVFX()
+		onDamagedSFX()
+		#endregion
 		if health.currentHealth <= 0:
 			character.queue_free()
 		#character.actionStateMach.states["Hurt"].knockbackDisplacement = knockback
@@ -48,3 +64,24 @@ func _startImmunity(delta):
 func _getCharacter(): ##Gets the parent character
 	while character is not Characters:
 		character = character.get_parent()
+
+
+#region OnHit VFX & SFX - TESTING
+func onDamagedVFX():
+	var black_fade = create_tween()
+	black_fade.tween_method(blackFade, 1.0, 0.0, 0.3)
+	damage_particles.emitting = true 
+	damage_particles_timer.start()
+
+func blackFade(newValue: float):
+	enemy_sprite.material.set_shader_parameter("value", newValue)
+	
+func _on_damage_particles_timer_timeout() -> void:
+	damage_particles.emitting = false
+
+func onDamagedSFX():
+	var random_sfx = sfx_list[randi() % sfx_list.size()]
+	on_hit_sfx.stream = random_sfx
+	on_hit_sfx.play()
+
+#endregion
