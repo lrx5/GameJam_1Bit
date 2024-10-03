2e89f453e1204135830ab903829e715a43dea462
lrx5 <143709045+lrx5@users.noreply.github.com>
2024-10-03 22:39:24 +0800

    Audio Manager Update

--- Scenes/Tower/CannonTower/cannonTower.gd
+++ Scenes/Tower/CannonTower/cannonTower.gd
@@ -4,8 +4,6 @@ extends Tower
 var tween
 @onready var cannon_sprite: Sprite2D = $GunPivot/CannonSprite
 
-
-
 func _process(delta: float) -> void:
 	if attackRange.target and attackCooldown(delta) and gunPivot.startShooting:
 		projectileSpawner.shoot()
