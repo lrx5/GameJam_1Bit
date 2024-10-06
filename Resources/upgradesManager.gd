fc77177749c98dc0f7cbf42def712855fa05054c
lrx5 <143709045+lrx5@users.noreply.github.com>
2024-10-06 18:54:56 +0800

    TowerShopHud Update
    - Working TowerShop Tween and Info
    - Updated Documentation
    - Added UpgradeManager Autoload

---
+++ Resources/upgradesManager.gd
@@ -0,0 +1,30 @@
+extends Node
+
+# TODO - Change Range Values!
+var turret_data = {
+	"cannon1": {"price": 20, "hp": 300, "fire_rate": 2, "damage": 8, "range": 0},
+	"cannon2": {"price": 20, "hp": 420, "fire_rate": 2, "damage": 12, "range": 0},
+	"cannon3": {"price": 60, "hp": 500, "fire_rate": 3, "damage": 13, "range": 0},
+	
+	"rocket1": {"price": 30, "hp": 200, "fire_rate": 0.5, "damage": 40, "range": 0},
+	"rocket2": {"price": 40, "hp": 200, "fire_rate": 0.5, "damage": 60, "range": 0},
+	"rocket3": {"price": 70, "hp": 250, "fire_rate": 0.5, "damage": 100, "range": 0},
+	
+	"beam1": {"price": 40, "hp": 100, "fire_rate": 8, "damage": 4, "range": 0},
+	"beam2": {"price": 50, "hp": 125, "fire_rate": 11, "damage": 4, "range": 0},
+	"beam3": {"price": 100, "hp": 150, "fire_rate": 15, "damage": 5, "range": 0},
+}
+
+func get_turret_stats(turret_type: String, level: int) -> Dictionary:
+	var key = turret_type + str(level)
+	if turret_data.has(key):
+		return turret_data[key]
+	else:
+		return {} 
+
+func get_specific_turret(turret_key: String) -> Dictionary:
+	if turret_data.has(turret_key):
+		return turret_data[turret_key]
+	else:
+		print("Turret level", turret_key, "not found")
+		return {}
--- Scenes/UI/Hotbar/towerInfo.gd
+++ Scenes/UI/Hotbar/towerInfo.gd
@@ -1,7 +1,6 @@
 extends PanelContainer
 
 @export var towerShop : PanelContainer
-
 @onready var stylebox = towerShop.get_theme_stylebox("panel") as StyleBoxFlat
 
 @onready var leftMargin 	= stylebox.content_margin_left
@@ -14,5 +13,45 @@ extends PanelContainer
 @onready var topBorder		= stylebox.border_width_top
 @onready var bottomBorder	= stylebox.border_width_bottom
 
+@onready var statsDictionary
+@onready var tower_name: Label = $InfoContainer/TowerName
+@onready var gold_cost: Label = $InfoContainer/GoldCost
+@onready var stats: Label = $InfoContainer/Stats
+
+
 func _ready() -> void:
-	global_position = towerShop.get_rect().position - Vector2(get_rect().size.x-leftMargin*4+leftBorder*5,0)
+	pass
+	#global_position = towerShop.get_rect().position - Vector2(get_rect().size.x-leftMargin*4+leftBorder*5,0)
+
+
+func _on_cannon_panel_mouse_entered() -> void:
+	setLabelInfo("cannon")
+	tower_name.text = "CANNON TOWER"
+	visible = true
+func _on_cannon_panel_mouse_exited() -> void:
+	visible = false
+
+
+func _on_beam_panel_mouse_entered() -> void:
+	setLabelInfo("beam")
+	tower_name.text = "BEAM TOWER"
+	visible = true
+func _on_beam_panel_mouse_exited() -> void:
+	visible = false
+
+
+func _on_rocket_panel_mouse_entered() -> void:
+	setLabelInfo("rocket")
+	tower_name.text = "ROCKET TOWER"
+	visible = true
+func _on_rocket_panel_mouse_exited() -> void:
+	visible = false
+
+
+func setLabelInfo(turret_name: String):
+	statsDictionary = UpgradesManager.get_turret_stats(turret_name, 1)
+	gold_cost.text = "Gold Cost : " + str(statsDictionary["price"])
+	stats.text = 	"Damage: " + str(statsDictionary["damage"]) + "\n" +\
+					"Range: " + str(statsDictionary["range"]) + "\n" +\
+					"Fire Rate: " + str(statsDictionary["fire_rate"]) + "\n" +\
+					"Health: " + str(statsDictionary["hp"])
